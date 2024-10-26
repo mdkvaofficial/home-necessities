991075e3db3c8374a9c7777f625be75167a03cf2
mdkvaofficial <mdanyalkayani.MDK@gmail.com>
2024-10-27 01:19:31 +0500

    language related, of course incomplete but mind changed to work for the core stuff first

---
+++ src/components/Language.svelte
@@ -0,0 +1,11 @@
+<script>
+    let language = {
+        en: {
+
+        },
+        ur:{
+
+        }
+    };
+    
+</script>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,4 @@
 <script>
-    import Nav from './../components/Nav.svelte'
+  import Nav from './../components/Nav.svelte'
 </script>
 <Nav/>
-
-Bismillah
