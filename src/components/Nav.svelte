b6658ed144f5afb12865092a837edd858ee87dda
mdkvaofficial <mdanyalkayani.MDK@gmail.com>
2024-10-26 11:27:06 +0500

    v1

---
+++ src/components/Nav.svelte
@@ -0,0 +1,31 @@
+<script>
+    
+  </script>
+  
+  <nav>
+    <!-- Navigation links or buttons go here -->
+    <a href="./">Home</a>
+    <a href="./mama">Mama</a>
+    <a href="./papa">Papa</a>
+    <a href="./behen">Behen</a>
+  </nav>
+  
+ 
+  
+  <style>
+    nav {
+      background-color: black;
+      color: white;
+      padding: 1em;
+      display: flex;
+      gap: 1em;
+    }
+  
+    nav a {
+      color: white;
+      text-decoration: none;
+    }
+  
+ 
+  </style>
+  
